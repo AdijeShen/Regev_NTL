43413044f51c01aa3035640f2984a23fa53baea4
adije <1534656015@qq.com>
2021-04-20 20:57:41 +0800

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adije/dev/Regev
-# Build directory: /home/adije/dev/Regev/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
